5da69154eb4d5209d7dc02fcbd670613a4c48987
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2019-06-16 14:35:32 -0300

    Traffic logic

--- trafficLight/traffic.ino
+++ trafficLight/traffic.ino
@@ -20,11 +20,25 @@
 //timer2 will interrupt at 8kHz
 
 //storage variables
-boolean toggle0 = 0;
-boolean toggle1 = 0;
-boolean toggle2 = 0;
+boolean redToggle = 0;
+boolean yellowToggle = 0;
+boolean greenToggle = 0;
+int count = 0;
+int timeCompare = 5;
+int mainRoadTimeOpen = 3;
+int mainRoadTimeTransition = 1;
+int mainRoadTimeClose = 2;
 
 void setup() {
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  timeCompare = mainRoadTimeClose;
+  redToggle = true;
   
   //set pins as outputs
   pinMode(8, OUTPUT);
@@ -32,19 +46,6 @@ void setup() {
   pinMode(13, OUTPUT);
 
   cli();//stop interrupts
-
-  //set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR2A register to 0
-  TCCR0B = 0;// same for TCCR2B
-  TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);   
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
 
   //set timer1 interrupt at 1Hz
   TCCR1A = 0;// set entire TCCR1A register to 0
@@ -59,61 +60,57 @@ void setup() {
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
-  //set timer2 interrupt at 8kHz
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
   //allow interrupts
   sei();
-
-}
-
-ISR(TIMER0_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
-//generates pulse wave of frequency 2kHz/2 = 1kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle0){
-    digitalWrite(8,HIGH);
-    toggle0 = 0;
-  }
-  else{
-    digitalWrite(8,LOW);
-    toggle0 = 1;
-  }
 }
 
 ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
 //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle1){
-    digitalWrite(13,HIGH);
-    toggle1 = 0;
-  }
-  else{
-    digitalWrite(13,LOW);
-    toggle1 = 1;
+  
+  count += 1;
+  if (count >= timeCompare) {
+    count = 0;
+    if (redToggle) {
+      redToggle = false;
+      greenToggle = true;
+      timeCompare = mainRoadTimeOpen;
+      return;
+    }
+    
+    if (greenToggle) {
+      greenToggle = false;
+      yellowToggle = true;
+      timeCompare = mainRoadTimeTransition;
+      return;
+    }
+    
+    if (yellowToggle) {
+      yellowToggle = false;
+      redToggle = true;
+      timeCompare = mainRoadTimeClose;
+      return;
+    }
   }
 }
   
-ISR(TIMER2_COMPA_vect){//timer1 interrupt 8kHz toggles pin 9
-//generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle2){
-    digitalWrite(9,HIGH);
-    toggle2 = 0;
-  }
-  else{
-    digitalWrite(9,LOW);
-    toggle2 = 1;
-  }
-}
-
-
 void loop(){
   //do other things here
+  if (redToggle) {
+	digitalWrite(13, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+  }
+  
+  if (greenToggle) {
+	digitalWrite(13, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, HIGH);
+  }
+  
+  if (yellowToggle) {
+	digitalWrite(13, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, LOW);
+  }
 }
+
